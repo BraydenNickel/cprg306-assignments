2bb38a97009283e51e3cf60c2342d67eea3acd4e
BraydenNickel <brayden.nickel@edu.sait.ca>
2023-10-02 20:20:36 -0600

    week3 v1.1

--- app/week3/page.js
+++ app/week3/page.js
@@ -2,7 +2,7 @@ import ItemList from "./item-list";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"> Week 3
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-indigo-950"> Week 3
       <div>
         <h1 className="text-4xl font-bold m-6 text-center text-purple-300">My Shopping List</h1>
         <ItemList />
